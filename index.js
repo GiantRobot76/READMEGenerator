cfed22ffce9c1f008715ef56c6f61077930a3354
Jon Bowman <jon.m.bowman@gmail.com>
2021-05-12 18:23:22 -0600

    added question array

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is the title of this project?",
+  "Write a brief description of the application:",
+  "Write any Applicable Installation Instructions:",
+  "Once Installed, How is the Application Used?",
+  "Under What License Is the Application Offered?",
+  "List Any Contributors To This Project",
+  "Describe Any Applicable Test Proecedures:",
+  "What E-Mail Should Be Included for Questions?",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
