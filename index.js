11a6ebf10c5290d2870d474faaa8550685ce1b53
Jon Bowman <jon.m.bowman@gmail.com>
2021-05-12 18:53:13 -0600

    Added Remainder of Text Inputs. Now Creates HTML Successfully

--- index.js
+++ index.js
@@ -41,81 +41,92 @@ function writeToFile() {
         choices: ["MIT", "Apache", "GNU"],
         name: "licenseID",
       },
+      {
+        type: "input",
+        message: questions[5],
+        name: "contrib",
+      },
+      {
+        type: "input",
+        message: questions[6],
+        name: "test",
+      },
+      {
+        type: "input",
+        message: questions[7],
+        name: "email",
+      },
     ])
     .then((response) => {
-      console.log(response.inputName);
-      console.log(response.desc);
-      console.log(response.licenseID);
-      //   const htmlSkeleton = `<!DOCTYPE html>
-      //   <html lang="en">
-      //     <head>
-      //       <meta charset="UTF-8" />
-      //       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      //       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      //       <link
-      //         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css"
-      //         rel="stylesheet"
-      //         integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0"
-      //         crossorigin="anonymous"
-      //       />
-      //     </head>
-      //     <body>
-      //       <title>Project Readme</title>
-      //       <div class="jumbotron bg-secondary">
-      //         <h1 class="display-4 text-center text-white" id="project-title">
-      //           Hello, world!
-      //         </h1>
-      //         <hr class="my-4" />
-      //       </div>
-      //       <div class="container border border-dark">
-      //         <div class="row border border-dark">
-      //           <h2>Description</h2>
-      //           <p id="description-text">Text Goes Here</p>
-      //         </div>
-      //         <div class="row border border-dark">
-      //           <h2>Table of Contents</h2>
-      //           <ol class="m-2">
-      //             <li><a href="#installation">Installation</a></li>
-      //             <li><a href="#usage">Usage</a></li>
-      //             <li><a href="#license">License</a></li>
-      //             <li><a href="#contributing">Contributing</a></li>
-      //             <li><a href="#tests">Tests</a></li>
-      //             <li><a href="#questions">Questions</a></li>
-      //           </ol>
-      //         </div>
-      //         <div class="row border border-dark">
-      //           <h2 id="installation">Installation</h2>
-      //           <p id="installation-text">Text Goes Here</p>
-      //         </div>
-      //         <div class="row border border-dark">
-      //           <h2 id="usage">Usage</h2>
-      //           <p id="usage-text">Text Goes Here</p>
-      //         </div>
-      //         <div class="row border border-dark">
-      //           <h2 id="license">License</h2>
-      //           <p id="license-text">Text Goes Here</p>
-      //         </div>
-      //         <div class="row border border-dark">
-      //           <h2 id="contributing">Contributing</h2>
-      //           <p id="contributing-text">Text Goes Here</p>
-      //         </div>
-      //         <div class="row border border-dark">
-      //           <h2 id="tests">Testing</h2>
-      //           <p id="test-text">Text Goes Here</p>
-      //         </div>
-      //         <div class="row border border-dark">
-      //           <h2 id="questions">Questions</h2>
-      //           <p id="questions-text">Contact Me Here with Questions!</p>
-      //           <p id="email">email</p>
-      //           <a href="https://github.com/GiantRobot76">My GitHub Page</a>
-      //         </div>
-      //       </div>
-      //     </body>
-      //   </html>`;
-
-      //   fs.writeFile("test.html", htmlSkeleton, (err) =>
-      //     err ? console.error(err) : console.log("File Created")
-      //   );
+      const htmlSkeleton = `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link
+              href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css"
+              rel="stylesheet"
+              integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0"
+              crossorigin="anonymous"
+            />
+          </head>
+          <body>
+            <title>Project Readme</title>
+            <div class="jumbotron bg-secondary">
+              <h1 class="display-4 text-center text-white" id="project-title">
+                ${response.inputName}
+              </h1>
+              <hr class="my-4" />
+            </div>
+            <div class="container border border-dark">
+              <div class="row border border-dark">
+                <h2>Description</h2>
+                <p id="description-text">${response.desc}</p>
+              </div>
+              <div class="row border border-dark">
+                <h2>Table of Contents</h2>
+                <ol class="m-2">
+                  <li><a href="#installation">Installation</a></li>
+                  <li><a href="#usage">Usage</a></li>
+                  <li><a href="#license">License</a></li>
+                  <li><a href="#contributing">Contributing</a></li>
+                  <li><a href="#tests">Tests</a></li>
+                  <li><a href="#questions">Questions</a></li>
+                </ol>
+              </div>
+              <div class="row border border-dark">
+                <h2 id="installation">Installation</h2>
+                <p id="installation-text">${response.install}</p>
+              </div>
+              <div class="row border border-dark">
+                <h2 id="usage">Usage</h2>
+                <p id="usage-text">${response.usage}</p>
+              </div>
+              <div class="row border border-dark">
+                <h2 id="license">License</h2>
+                <p id="license-text">Offered Under the ${response.licenseID} License</p>
+              </div>
+              <div class="row border border-dark">
+                <h2 id="contributing">Contributing</h2>
+                <p id="contributing-text">Text Goes Here</p>
+              </div>
+              <div class="row border border-dark">
+                <h2 id="tests">Testing</h2>
+                <p id="test-text">${response.test}</p>
+              </div>
+              <div class="row border border-dark">
+                <h2 id="questions">Questions</h2>
+                <p id="questions-text">Contact Me Here with Questions!</p>
+                <p id="email">${response.email}</p>
+                <a href="https://github.com/GiantRobot76">My GitHub Page</a>
+              </div>
+            </div>
+          </body>
+        </html>`;
+      fs.writeFile("index.html", htmlSkeleton, (err) =>
+        err ? console.error(err) : console.log("File Created")
+      );
     });
 }
 
